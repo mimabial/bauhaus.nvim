d7117548a514f6d9f19d6a052f9d84c08d7f3700
mimabial <mabialamirion@hotmail.fr>
2025-09-09 02:32:02 +0200

    cp

--- lua/cyberdream/colors.lua
+++ lua/cyberdream/colors.lua
@@ -26,16 +26,16 @@ M.default = {
   bg_alt = "#ccc0d6",       -- Slightly darker variant of background
   bg_highlight = "#bba8c9", -- Even darker for highlights
   fg = "#060606",           -- Dark foreground for readability (matches kitty)
-  grey = "#8B7A9C",         -- Dreamy lavender-grey
-  blue = "#6B85E6",         -- Soft dreamy blue
-  green = "#66B599",        -- Dreamy mint-green
-  cyan = "#7AB8CC",         -- Soft sky cyan
-  red = "#E6668A",          -- Dreamy rose-red
-  yellow = "#E6CC7A",       -- Soft golden yellow
-  magenta = "#B87ACC",      -- Dreamy purple-magenta
-  pink = "#E699B8",         -- Soft dreamy pink
-  orange = "#E6A366",       -- Soft peachy orange
-  purple = "#9966CC",       -- Dreamy lavender purple
+  grey = "#4A4066",         -- Dark cyberpunk purple-grey
+  blue = "#00CCFF",         -- Electric neon cyan-blue
+  green = "#00FF41",        -- Matrix green
+  cyan = "#00FFCC",         -- Neon cyan
+  red = "#FF0066",          -- Hot neon pink-red
+  yellow = "#FFFF00",       -- Electric yellow
+  magenta = "#FF00CC",      -- Neon magenta
+  pink = "#FF0099",         -- Hot pink
+  orange = "#FF6600",       -- Electric orange
+  purple = "#6600FF",       -- Deep neon purple
 }
 
 
