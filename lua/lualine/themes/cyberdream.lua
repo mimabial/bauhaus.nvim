d2dc933976b79cc13c555077c56c18d7faee7ed5
mimabial <mabialamirion@hotmail.fr>
2025-09-18 05:41:49 +0200

    removed auto light

--- lua/lualine/themes/cyberdream.lua
+++ lua/lualine/themes/cyberdream.lua
@@ -2,41 +2,37 @@
 local colors = require("cyberdream.colors").default
 local opts = require("cyberdream.config").options
 
-if opts.variant == "light" then
-    colors = require("cyberdream.colors").light
-end
-
-if opts.variant == "auto" then
-    if vim.o.background == "light" then
-        colors = require("cyberdream.colors").light
-    end
+if opts.variant == "bauhaus" then
+  colors = require("cyberdream.colors").bauhaus
+elseif opts.variant == "bluesky" then
+  colors = require("cyberdream.colors").bluesky
 end
 
 if opts.transparent then
-    colors.bg = "NONE"
+  colors.bg = "NONE"
 end
 
 local cyberdream = {
-    normal = {
-        a = { fg = colors.blue, bg = colors.bg },
-        b = { fg = colors.cyan, bg = colors.bg },
-        c = { fg = colors.fg, bg = colors.bg },
-        x = { fg = colors.fg, bg = colors.bg },
-        y = { fg = colors.magenta, bg = colors.bg },
-        z = { fg = colors.grey, bg = colors.bg },
-    },
-    insert = {
-        a = { fg = colors.green, bg = colors.bg },
-        z = { fg = colors.grey, bg = colors.bg },
-    },
-    visual = {
-        a = { fg = colors.magenta, bg = colors.bg },
-        z = { fg = colors.grey, bg = colors.bg },
-    },
-    terminal = {
-        a = { fg = colors.orange, bg = colors.bg },
-        z = { fg = colors.grey, bg = colors.bg },
-    },
+  normal = {
+    a = { fg = colors.blue, bg = colors.bg },
+    b = { fg = colors.cyan, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
+    x = { fg = colors.fg, bg = colors.bg },
+    y = { fg = colors.magenta, bg = colors.bg },
+    z = { fg = colors.grey, bg = colors.bg },
+  },
+  insert = {
+    a = { fg = colors.green, bg = colors.bg },
+    z = { fg = colors.grey, bg = colors.bg },
+  },
+  visual = {
+    a = { fg = colors.magenta, bg = colors.bg },
+    z = { fg = colors.grey, bg = colors.bg },
+  },
+  terminal = {
+    a = { fg = colors.orange, bg = colors.bg },
+    z = { fg = colors.grey, bg = colors.bg },
+  },
 }
 
 return cyberdream
